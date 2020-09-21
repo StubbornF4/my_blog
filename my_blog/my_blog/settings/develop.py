c2b79885d31440292e8e0764110f7d40c69a2be4
john <lichunxiao0404@163.com>
2020-09-21 17:27:33 +0800

    更改settings

--- my_blog/my_blog/settings/develop.py
+++ my_blog/my_blog/settings/develop.py
@@ -1,10 +1,12 @@
 from .base import * # NOQA
 
 
-DEBUG = True
+DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+ALLOWED_HOSTS = ['158.247.194.175']
